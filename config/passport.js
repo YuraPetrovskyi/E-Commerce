524cc6678a373cabd37d017cf3d9cebd081cc148
Yurii Petrovskyi <yurakarpaty@gmail.com>
2024-01-16 22:33:32 +0000

    add functions findByEmail and FindById to passport

--- config/passport.js
+++ config/passport.js
@@ -1,42 +1,42 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require("bcrypt");
 
-const userDB = {
-  id: 1,
-  username: 'yura',
-  password: '111'
-};
+
+const find = require('../db/find_in_passprt');
+
+
+// const userDB = {
+//   id: 1,
+//   username: 'yura',
+//   email: 'yura@gmail.com',
+//   password: '111'
+// };
 
 passport.use(new LocalStrategy(
-  (username, password, done) => {
-    if (username === userDB.username && password === userDB.password) {
-      console.log('pasport: name and password ok');
-      return done(null, userDB);
-    } else {
-      console.log('pasport: name and password NOT ok');
-      return done(null, false);
-    }
+  {usernameField: 'email'},
+  (email, password, done) => {   
+    find.findByEmail(email, async (err, user) => { // Look up user in the db  
+      
+      if(err) return done(err);   
+               // перевіряє, чи знайдено помилку. If there's an error in db lookup,return err callback function
+      
+      if(!user) return done(null, false, { message: 'Incorrect username.' });  // якщо НЕ знайдено жодного користувача, done()зворотний виклик з аргументами, які показують, що помилки НЕ було і НЕ знайдено ЖОДНОГО користувача.
+      
+      const matchedPassword = await bcrypt.compare(password, user.password);
+      if(!matchedPassword) return done(null, false, { message: 'Incorrect password.' });        // перевіряє, чи знайдено користувача, але пароль недійсний.
+      
+      return done(null, user)             // Повертає done()функцію зворотного виклику з аргументами, які показують, що помилки НЕ було, і користувача знайдено.
+    });
   }
 ));
 
-// passport.serializeUser((user, done) => {
-//   console.log('Serialize user passport local', user);
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser((id, done) => {
-//   console.log('Deserialize user passport local', id);
-//   // const user = (userDB.id === id) ? userDB : false;
-//   // console.log('Deserialize user data: ', user)
-//   done(null, user);
-// });
-
 passport.serializeUser((user, done) => {
-  console.log('Serialize user passport local', user);
-  done(null, user.id);
+  console.log('Serialize user passport local:', user);
+  done(null, user);
 });
 
 passport.deserializeUser((user, done) => {
-  console.log('Deserialize user passport local', user);
+  console.log('Deserialize user passport local:', user);
   done(null, user);
 });
---
+++ db/find_in_passprt.js
@@ -0,0 +1,48 @@
+const { pool } = require('./pool');
+
+
+function findByEmail(email, callback) {
+  try {
+    pool.query('SELECT * FROM users WHERE email = $1', [email], (error, results) => {
+      if (error) {
+        return callback(error, null);
+      }
+      if (results.rows.length === 0) {
+        console.log('findByUsername function can not find the names user!');
+        return callback(null, false);
+      }    
+      const user = results.rows[0];
+      // console.log('function findByUsername: ', user)
+      // console.log(results)
+
+      return callback(null, user);
+    });
+  } catch (error) {
+    // Обробка помилки, якщо вона виникає під час виконання запиту до бази даних
+    return callback(error, null);
+  }
+}
+
+function findById(id, callback) {
+  try {
+    pool.query('SELECT * FROM users WHERE user_id = $1', [id], (error, results) => {
+      if (error) {
+        return callback(error, null);
+      }
+      if (results.rows.length === 0) {
+        return callback(null, false);
+      }
+      const user = results.rows[0];
+      console.log('function findById: ', user)
+      return callback(null, user);
+    });
+  } catch (error) {
+    // Обробка помилки, якщо вона виникає під час виконання запиту до бази даних
+    return callback(error, null);
+  }
+}
+
+module.exports = {
+  findByEmail,
+  findById,
+};
