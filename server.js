caa174beeb1b70c630ce2b5f67e4db4cb22f7424
Yurii Petrovskyi <yurakarpaty@gmail.com>
2024-03-13 19:37:24 +0000

    providing appropriate CORS headers

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', WEB_APP_URL);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 // app.use(express.json()); //Цей рядок дозволяє обробляти запити, які мають тип application/json
 app.use(express.urlencoded({ extended: false })); //Цей рядок дозволяє обробляти запити, які мають тип application/x-www-form-urlencoded
 app.use(express.json({
